4db4c771774c1674a34e8c054245fd2d36d13b49
JonSwallow <joncswallow@gmail.com>
2018-03-10 16:45:25 -0700

    Changelog4A
    Not really amused at the technicalities that prevent jpg and JPG from being corrected.

--- hw-6.js
+++ hw-6.js
@@ -28,7 +28,7 @@ To begin, you should select the body element from the DOM, then remove its exist
 */
 //test
 var myImage = new Image (882, 501);
-myImage.src = "images/stevi.JPG"
+myImage.src = "images/gitsucks.jpg"
 
 let body = document.body;
 body.innerText = "";
